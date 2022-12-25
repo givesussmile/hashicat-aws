9cbccf4cc740aed425983dc19b686a9ae9d8f48f
givesussmile <41364470+givesussmile@users.noreply.github.com>
2022-12-25 21:32:58 +0900

    Create 3s-bucket.tf

---
+++ 3s-bucket.tf
@@ -0,0 +1,14 @@
+
+bucket_prefix="hoon"
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
